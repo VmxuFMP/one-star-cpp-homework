677b504f9cccbb4220b39188c7ccad3744ee9eaf
VmxuFMP <a1145508@mail.nuk.edu.tw>
2026-03-26 01:07:15 +0800

    2026/03/26 提交

--- Problem/UVa_10101.cpp
+++ Problem/UVa_10101.cpp
@@ -23,8 +23,8 @@ void bangla(long long n) {
 		printf(" shata");
 		n %= 100;
 	}
-	if (n > 0 || (n == 0 && n == -1)) {
-		if (n > 0) printf(" %lld", n);
+	if (n > 0) {
+		printf(" %lld", n);
 	}
 }
 
